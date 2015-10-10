b23b571e1b05192586880fd882cb367d424c3cd1
appledead <djovas@rambler.ru>
2015-10-10 12:07:28 +0300

    Create stdafx.h

---
+++ stdafx.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <conio.h>
+#include <cmath>
+#include <math.h>
+
+#define N 10
+#define D 2
+
+using namespace std;
+
+int search_center(int& a, int& b, float& k,float * mas);
+int search_delenie(float * arr, float k, int a, int b);
